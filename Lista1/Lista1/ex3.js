63e0a02c119d08e6cebfdcac9cf12bf2abeab979
Luan Elder <luaneldermendes@gmail.com>
2023-10-19 20:22:00 -0300

    correção, duplicada Lista1

--- Lista1/Lista1/ex3.js
+++
@@ -1,7 +0,0 @@
-const lado = 6;
-const area = lado * lado;
-
-
-
-
-console.log("Area do quadrado em cm:", area);
--- Lista1/Lista1/ex4.js
+++
@@ -1,6 +0,0 @@
-const celsius = 30;
-const fahrenheit = (celsius * 1.8) + 32;
-
-
-
-console.log("Graus em fahrenheit:", fahrenheit);
